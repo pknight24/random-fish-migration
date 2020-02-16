4706fe814127f723e095e21d8899374e9c5e6bdb
Parker Knight <pknight24@protonmail.com>
2020-02-16 10:59:12 -0500

    Add nt option to main

--- main.m
+++ main.m
@@ -1,9 +1,8 @@
-function [results] = main(nsims = 100)
+function [results] = main(nsims = 100, nt = 50)
   load("initial_temp_2013");
   initial_temps = initial_temp_2013(:,7);
-  opt = 12.5
+  opt = 12.5;
   R = 0.05;
-  nt = 50;
   results = zeros(length(initial_temps), 15);
   for i = 1:15
     initial_node = i;
