d0001e497cc1cd9d31c0f29ed21cc01d8bf575f8
Parker Knight <pknight24@protonmail.com>
2020-02-17 20:29:05 -0500

    Add final submission

--- simulate.m
+++ simulate.m
@@ -7,19 +7,6 @@ function [final] = simulate(initial_node, initial_temps, opt, nt, R)
   for t = 1:nt
 
     prob_vec = P * prob_vec; % now all we care about is the probability vector
-    % interval_vector = cumsum(prob_vector);
-    % guess = rand(1);
-    % for i = 1:n
-    %   if i == 1
-    % 	if guess < interval_vector(i)
-    % 	  next_node = 1;
-    % 	end
-    %   elseif (interval_vector(i - 1) < guess) && (guess < interval_vector(i))
-    % 	next_node = i;
-    %   end
-    % end
-    % position_vec = zeros(n,1);
-    % position_vec(next_node) = 1;
 	  
     % now update the temperatures and the P matrix
     current_temps = update_temps(current_temps, R);
